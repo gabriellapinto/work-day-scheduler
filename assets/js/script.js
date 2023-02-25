0e654ff7cff28350fcf12134a17650b01c1af9fd
Gabby <gabbympinto@gmail.com>
2023-02-25 15:04:17 -0500

    added time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,18 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
+    $('.saveBtn').on('click', function() {
+      var timeBlockId = $(this).parent().attr('id');
+      var userInput = $(this).siblings('.description').val();
+      localStorage.setItem(timeBlockId, userInput);
+    });
+
+    $('.time-block').each(function() {
+      var timeBlockId = $(this).attr('id');
+      var userInput = localStorage.getItem(timeBlockId);
+      $(this).find('.description').val(userInput);
+    })
+
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,4 +32,6 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    var today = dayjs();
+    $('#currentDay').text(today.format('dddd, MMMM D'));
   });
